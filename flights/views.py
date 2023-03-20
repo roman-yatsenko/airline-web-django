63ae52da3ecdcbcad4fbefd702484de80b70259b
YaRo <roman.yatsenko@hneu.net>
2023-03-20 15:32:04 +0200

    Add flight page

--- flights/views.py
+++ flights/views.py
@@ -8,3 +8,9 @@ def index(request):
     return render(request, 'flights/index.html', context={
         'flights': Flight.objects.all(),
     })
+
+def flight(request, flight_id):
+    return render(request, 'flights/flight.html', context={
+        'flight': Flight.objects.get(pk=flight_id),
+    })
+    
