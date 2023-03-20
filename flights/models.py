23d728210262df1c3ca44f397bbf0ce84b44cd01
YaRo <roman.yatsenko@hneu.net>
2023-03-20 15:22:00 +0200

    Add admin config

--- flights/models.py
+++ flights/models.py
@@ -5,6 +5,9 @@ class Airport(models.Model):
     code = models.CharField(max_length=3)
     city = models.CharField(max_length=64)
 
+    def __str__(self):
+        return f'{self.code}'
+
 
 class Flight(models.Model):
     origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='departures')
--- flights/views.py
+++ flights/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Airport, Flight
+
 # Create your views here.
+
+def index(request):
+    return render(request, 'flights/index.html', context={
+        'flights': Flight.objects.all(),
+    })
