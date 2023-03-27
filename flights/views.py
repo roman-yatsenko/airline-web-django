13b7fd41d060465bbcbeb4cef7d0e2e466aa03d3
YaRo <roman.yatsenko@hneu.net>
2023-03-27 15:10:54 +0300

    Add Passenger model

--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import Airport, Flight
+from .models import Airport, Flight, Passenger
 
 # Create your views here.
 
@@ -10,7 +10,19 @@ def index(request):
     })
 
 def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    passengers = flight.passengers.all()
+    non_passengers = Passenger.objects.exclude(flights=flight).all()
     return render(request, 'flights/flight.html', context={
-        'flight': Flight.objects.get(pk=flight_id),
+        'flight': flight,
+        'passengers': passengers,
+        'non_passengers': non_passengers,
     })
     
+def book(request, flight_id):
+    if request.method == 'POST':
+        flight = Flight.objects.get(id=flight_id)
+        passenger_id = int(request.POST['passenger'])
+        passenger = Passenger.objects.get(id=passenger_id)
+        flight.passengers.add(passenger)
+    return redirect('flight', flight_id=flight_id)
